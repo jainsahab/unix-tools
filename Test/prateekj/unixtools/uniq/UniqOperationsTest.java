a06a0c5d02208fc59c035f45ba469abdda8c964d
Prateek <Prateekj@thoughtworks.com>
2014-01-10 00:13:40 +0530

    Uniq Completed

--- Test/prateekj/unixtools/uniq/UniqOperationsTest.java
+++ Test/prateekj/unixtools/uniq/UniqOperationsTest.java
@@ -14,4 +14,31 @@ public class UniqOperationsTest {
         String actual = u.getUniqueLines(fileData);
         assertEquals(expected,actual);
     }
+
+    @Test
+    public void testGetUniqueLines2() throws Exception {
+        String fileData ="Prateek\nPrayas\nPrayas";
+        String expected ="Prateek\nPrayas";
+        UniqOperations u = new UniqOperations();
+        String actual = u.getUniqueLines(fileData);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testGetUniqueLines3() throws Exception {
+        String fileData ="Prateek\nPrayas\nKajal";
+        String expected ="Prateek\nPrayas\nKajal";
+        UniqOperations u = new UniqOperations();
+        String actual = u.getUniqueLines(fileData);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testGetUniqueLines4() throws Exception {
+        String fileData ="Prateek\nPrayas\nKajal\nKajal";
+        String expected ="Prateek\nPrayas\nKajal";
+        UniqOperations u = new UniqOperations();
+        String actual = u.getUniqueLines(fileData);
+        assertEquals(expected,actual);
+    }
 }
--- src/prateekj/unixtools/uniq/Uniq.java
+++ src/prateekj/unixtools/uniq/Uniq.java
@@ -4,6 +4,8 @@ import prateekj.fs.*;
 public class Uniq {
     public static void main(String[] args) {
         FileOperations fs = new FileOperations();
+        UniqOperations u = new UniqOperations();
         String content = fs.readFile(args[0]);
+        System.out.println(u.getUniqueLines(content));
     }
 }
