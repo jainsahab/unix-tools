1734c5018c1a1dc3a820205cca447d0907f55a3c
Prateek <Prateekj@thoughtworks.com>
2014-01-14 20:10:29 +0530

    Sort is working on user defined comparator

--- src/prateekj/unixtools/sort/SortOperation.java
+++ src/prateekj/unixtools/sort/SortOperation.java
@@ -2,13 +2,14 @@ package prateekj.unixtools.sort;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 
 class Options {
     String fileName;
     boolean reverse;
 }
 
-public class SortOperation {
+public class SortOperation{
     Options getOptions(String[] temp) {
         Options op = new Options();
         for (String s : temp) {
@@ -23,10 +24,22 @@ public class SortOperation {
     String sortContent(String content, Options input) {
         String sorted[] = content.split("\n");
         String result = "";
-        if (input.reverse)
-            Arrays.sort(sorted, Collections.reverseOrder());
-        else
-            Arrays.sort(sorted);
+        if (input.reverse){
+            Arrays.sort(sorted, new Comparator<String>() {
+                @Override
+                public int compare(String p1, String p2) {
+                    return p1.compareTo(p2) * -1;
+                }
+            });
+        }
+        else{
+            Arrays.sort(sorted, new Comparator<String>() {
+                @Override
+                public int compare(String p1, String p2) {
+                    return p1.compareTo(p2);
+                }
+            });
+        }
         for (String s : sorted) {
             result += s + "\n";
         }
